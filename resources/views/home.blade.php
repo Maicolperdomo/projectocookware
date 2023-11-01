6f6501d4db59594be19ef3206ae798063849a257
Maicol Perdomo <maicolfabianp79@gmail.com>
2023-11-01 18:52:25 -0500

    l

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/bootstrap.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="./assets/Js/bootstrap.js"></script>
-    <script src="./assets/Js/axios.min.js"></script>
-    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         .vl {
             border-left: 6px solid green;
@@ -24,48 +20,47 @@
 </head>
 
 <body class="container-fluid">
+    <div class="container-fluid">
+        <nav class="navbar">
+            <nav class="navbar">
+                <div class="container-fluid">
+                  <h1 class="navbar-brand" href="#">
+                    <img src="../image/logosmall.png" alt="Logo" width="80" height="80" class="d-inline-block align-text-center">
+                    Cookware
+                  </h1>
+                </div>
+              </nav>
+            <div clas="d-flex">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                  </form>
+            </div>
+        </nav>
+    </div>
     <div class="row">
-        <div class="col-3" style="background-color: white">
+        <div class="col-3 bg-warning">
             <div>
-                <a href="#">
-                    <img src="../image/logosmall.png" alt="Logo" width="70" height="70">
-                </a>
-                <h1 class="d-inline-block align-text-center mx-3">Cookware</h1>
+                <img src="../image/61205.png" alt="..." style="width: 200px">
                 <div>
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                        </ul>
-                        <div class="card-body">
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                    </div>
+                    <h5 class="card-title d-flex justify-contrn-cerer">@Nickname</h5>
+                </div>
+                <ul>
+                    <li class="list-group-item">Cantidad de Recetas</li>
+                    <li class="list-group-item">Seguidores</li>
+                    <li class="list-group-item">Calificacion</li>
+                </ul>
+                <div>
+                    <a href="#">Actualizar Perfil</a>
+                    <a href="#">Cerrar sesion</a>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-9 right" style="background-color: #C0DD8D">
-        <div class="row d-flex justify-content-center">
-            <div class="mx-3">
-                <input class="form-control" type="search" placeholder="Buscar receta" aria-label="Search">
-            </div>
-            <div>
-                <button class="btn btn-outline-success align-content-center" type="submit">Buscar</button>
-            </div>
+        <div class="col-9 right bg-primary">
+            <h1>hola</h1>
         </div>
-        <div></div>
     </div>
-    </div>
-</body>
+</body> 
 
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
