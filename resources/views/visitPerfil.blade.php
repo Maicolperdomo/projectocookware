4292ba8afb769c0168fa8fcbba975518063f1371
santiago <santiagosanchezramirez9@gmail.com>
2023-11-01 18:51:48 -0500

    iniciar

--- resources/views/visitPerfil.blade.php
+++ resources/views/visitPerfil.blade.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <h1>Hola vista visitarPerfil</h1> 
-</body>
-</html>
+<div class="row">
+    <div class="col-md-12">
+        <h1>Visitar perfil</h1>
+
+        <form action="{{ route('perfil.update') }}" method="POST" onsubmit="return validar()">
+            @csrf
+
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name" class="form-control"  required>
+            </div>
+            <div class="form-group">
+                <button type="button" class="btn btn-primary" id="boton-visitar-perfil">Visitar perfil</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script>
+    function validar() {
+        // Validamos que el formulario esté completo
+        if ($('#name').val() === '' || $('#email').val() === '') {
+            alert('El formulario no está completo');
+            return false;
+        }
+
+        // Simulamos el evento de clic en el botón de cancelar
+        $('#cancelar').click();
+
+        // Devolvemos true para que el formulario se envíe
+        return true;
+    }
+
+    // Al presionar click en el botón de visitar perfil, redirigimos al usuario a la página de perfil
+    $('#boton-visitar-perfil').click(function() {
+        window.location.href = "{{ route('perfil.index') }}";
+    });
+</script>
--- resources/views/visitReceta.blade.php
+++ resources/views/visitReceta.blade.php
@@ -1,11 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <h1>Hola vista visitarReceta</h1> 
-</body>
-</html>
+<div class="row">
+    <div class="col-md-12">
+        <h1>VistarReceta</h1>
+
+        <form action="{{ route('perfil.update') }}" method="POST" onsubmit="return validar()">
+            @csrf
+
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <a href="{{ route('recetas.show', $receta->id) }}" class="btn btn-secondary">Ver receta</a>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script>
+    function validar() {
+        // Validamos que el formulario esté completo
+        if ($('#name').val() === '' || $('#email').val() === '') {
+            alert('El formulario no está completo');
+            return false;
+        }
+
+        // Simulamos el evento de clic en el botón de cancelar
+        $('#cancelar').click();
+
+        // Devolvemos true para que el formulario se envíe
+        return true;
+    }
+
+    // Al presionar click en el botón de ver receta, redirigimos al usuario a la página de receta
+    $('#boton-ver-receta').click(function() {
+        window.location.href = "{{ route('recetas.show', $receta->id) }}";
+    });
+
+    // Al presionar enter en el botón de ver receta, redirigimos al usuario a la página de receta
+    $('#boton-ver-receta').keydown(function(e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            window.location.href = "{{ route('recetas.show', $receta->id) }}";
+        }
+    });
+</script>
