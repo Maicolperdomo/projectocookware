e8e9a27369a530e37dd0c50cad1550727cd71d39
Mayra Solano <alejandrasol89023@gmail.com>
2023-11-08 22:15:52 -0500

    actualizar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COOKWARE/Home</title>
     <link rel="shortcut icon" href="../image/logosmall.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="../Js/axios.min.js"></script>
@@ -67,62 +68,68 @@
                 </div>
             </div>
         </div> --}}
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-              <div class="card h-50">
-                <img src="../image/tarta.jpg"  class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">NOMBRE RECETA</h5>
-                  <p class="card-text">DESCRIPCION</p>
-                  <div class="d-flex justify-content-around">
-                    <a class="btn btn-primary" href="visitarPerfil">Visitar Perfil</a>
-                    <a class="btn btn-primary" onclick="abrir()">Publicar Receta</a>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-50">
+                        <img src="../image/tarta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">NOMBRE RECETA</h5>
+                            <p class="card-text">DESCRIPCION</p>
+                            <div class="d-flex justify-content-around">
+                                <a class="btn btn-primary" href="visitarPerfil"><i
+                                        class="fa-solid fa-eye-low-vision fa-bounce"></i></a>
+                                <a class="btn btn-primary" onclick="abrir()"><i
+                                        class="fa-sharp fa-solid fa-upload fa-bounce"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div class="col">
+                    <div class="card hF-50">
+                        <img src="../image/tarta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">NOMBRE RECETA</h5>
+                            <p class="card-text">DESCRIPCION</p>
+                            <div class="d-flex justify-content-around">
+                                <a class="btn btn-primary" href="visitarPerfil"><i
+                                        class="fa-solid fa-eye-low-vision fa-bounce"></i></a>
+                                <a class="btn btn-primary" onclick="abrir()"><i
+                                        class="fa-sharp fa-solid fa-upload fa-bounce"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                <div class="col">
+                    <div class="card h-50">
+                        <img src="../image/tarta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">NOMBRE RECETA</h5>
+                            <p class="card-text">DESCRIPCION</p>
+                            <div class="d-flex justify-content-around">
+                                <a class="btn btn-primary" href="visitarPerfil"><i
+                                        class="fa-solid fa-eye-low-vision fa-bounce"></i></a>
+                                <a class="btn btn-primary" onclick="abrir()"><i
+                                        class="fa-sharp fa-solid fa-upload fa-bounce"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-50">
+                        <img src="../image/tarta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">NOMBRE RECETA</h5>
+                            <p class="card-text">DESCRIPCION</p>
+                            <div class="d-flex justify-content-around">
+                                <a class="btn btn-primary" href="visitarPerfil"><i
+                                        class="fa-solid fa-eye-low-vision fa-bounce"></i></a>
+                                <a class="btn btn-primary" onclick="abrir()"><i
+                                        class="fa-sharp fa-solid fa-upload fa-bounce"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col">
-              <div class="card h-50">
-                <img src="../image/tarta.jpg"  class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">NOMBRE RECETA</h5>
-                  <p class="card-text">DESCRIPCION</p>
-                  <div class="d-flex justify-content-around">
-                    <a class="btn btn-primary" href="visitarPerfil">Visitar Perfil</a>
-                    <a class="btn btn-primary" onclick="abrir()">Publicar Receta</a>
-                </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card h-50">
-                <img src="../image/tarta.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">NOMBRE RECETA</h5>
-                  <p class="card-text">DESCRIPCION</p>
-                  <div class="d-flex justify-content-around">
-                    <a class="btn btn-primary" href="visitarPerfil">Visitar Perfil</a>
-                    <a class="btn btn-primary" onclick="abrir()">Publicar Receta</a>
-                </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card h-50">
-                <img src="../image/tarta.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">NOMBRE RECETA</h5>
-                  <p class="card-text">DESCRIPCION</p>
-                  <div class="d-flex justify-content-around">
-                    <a class="btn btn-primary" href="visitarPerfil">Visitar Perfil</a>
-                    <a class="btn btn-primary" onclick="abrir()">Publicar Receta</a>
-                </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-</div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
