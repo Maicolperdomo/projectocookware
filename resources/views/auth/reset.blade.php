d40cba90e2a0872e3c80527b0f593f47b0e0c5ca
Valentina Quiza <vquiza42@gmail.com>
2023-11-16 18:32:47 -0500

    ac

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cambiar contraseña</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body style="background-color: #c0dd8d;">
     <div class="container">
@@ -29,7 +31,7 @@
                                 </div>
                                 <div class="col-12">
                                     <label for="password" class="form-label"> Confirmar contraseña</label>
-                                    <input type="password" name="email" class="form-control" id="txtCpassword" required>
+                                    <input type="password" name="password" class="form-control" id="txtCpassword" required>
                                 </div>
                                 <div class="col-12">
                                     <button class="btn btn-success " type="submit">enviar</button>
