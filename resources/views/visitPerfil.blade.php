73a0955d86644272a31995dd497effc316f13ff9
santiago <santiagosanchezramirez9@gmail.com>
2023-11-17 13:35:42 -0500

    romper

--- resources/views/visitPerfil.blade.php
+++ resources/views/visitPerfil.blade.php
@@ -1,21 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Cookware </title>
+    <title>COOKWARE/Home</title>
     <link rel="shortcut icon" href="../image/logosmall.png" type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="../Js/axios.min.js"></script>
     <style type="text/css" id="operaUserStyle"></style>
+
 </head>
-<body class="container-fluid">
-    <div class="row">
-    <div class="col-3" style="height: 606px;">
+    <body class="container-fluid">
+        <div class="row">
+            <div class="col-3" style="height: 606px;">
                 <img src="../image/logo.png" alt="Logo" width="300" height="100">
                 <div class="d-flex justify-content-center">
-                    <img src="../image/61205.png" alt="..." style="width: 200px">
+                    <img src="../image/3135768.png" alt="..." style="width: 160px">
                 </div>
                 <div class="my-3">
                     <div class="d-flex justify-content-center">
@@ -26,41 +26,31 @@
                         <li class="list-group-item">Seguidores: </li>
                         <li class="list-group-item">Calificacion: </li>
                     </ul>
-          
+                    <div class="d-flex align-items-end justify-content-center" style="height: 140px">
+                        <div class="btn-group dropup" role="group">
+                        </div>
                     </div>
                 </div>
-            </div>
-    </div>
-              <div class="col-9 right my-3" style="background-color: #dc3545">
-               <div class="container">
-             <div class="form-container">
-              <label for="uname">Buscar</label>
-              <input type="text" name="uname" required>
-             <head>
-             <meta charset="UTF-8">
-             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-             <title>Document</title>
-</head>
-
-<body>
-                <div class="row d-flex justify-content-around mt-4" style="background-color: #dc3545"> 
+            </div> 
+            <div class="col-9 right my-3" style="background-color: white">
+                <div class="row d-flex justify-content-around mt-4" style="background-color: #dc3545">
+                <div class="feed-tip-ficha-horizontal">
                     <div class="card" style="width: 18rem;">
                         <div class="d-flex justify-content-center">
-                        <img src="../image/61205.png" alt="..." width="200" height="200">
-                    </div>
-                        <div class="card-body">
-                            <h5 class="card-title">NOMBRE RECETA</h5>
-                            <p class="card-text">DESCRIPCION</p>
-                        </div>
-                        <div class="card-body">
-                            <a href="visitPerfil" class="card-link">Visitar Perfil</a>
-                            <a href="visitarReceta" class="card-link">Visitar Receta</a>
-                   </div>
+                            <img src="../image/61205.png" alt="..." width="90" height="100">
+                              </div>
+                            <div class="my-3">
+                          <div class="d-flex justify-content-center">
+                           <h5 class="card-title">@ santi_san</h5>
+                           </div>
+                           <ul class="text-left">
+                          <li class="list-group-item">Calificacion: </li>
+                       </ul>
             </div>
-    </div>
- </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"> </script>
+       </div>
+     </div>
+  </div> 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
 </body>
 </html>
