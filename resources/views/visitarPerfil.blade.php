c62afa3a0270930e02d1f7e996118a692739117f
Valentina Quiza <vquiza42@gmail.com>
2023-11-22 22:25:15 -0500

    a

--- resources/views/visitarPerfil.blade.php
+++ resources/views/visitarPerfil.blade.php
@@ -42,22 +42,55 @@
             <div class="col-9 right my-3" style="background-color: white">
                 <div class="row d-flex justify-content-around mt-4" style="background-color: #dc3545">
                 <div class="feed-tip-ficha-horizontal">
-                    <div class="card" style="width: 18rem;">
-                        <div class="d-flex justify-content-center">
+                   <!-- <div class="card" style="width: 18rem;">
+                       <div class="d-flex justify-content-center">
                             <img src="../image/61205.png" alt="..." width="90" height="100">
-                              </div>
+                              </div>-->
                             <div class="my-3">
                             <div class="my-3">
-                    <div class="d-flex justify-content-center">
+                   <!-- <div class="d-flex justify-content-center">
                         <h5 class="card-title">@ {{ auth()->user()->nickname }}</h5>
-                    </div>
-                           <ul class="text-left">
-                          <li class="list-group-item">Calificacion: </li>
-                       </ul>
-            </div>
+                    </div>-->
+                          
+                         <!-- <li class="list-group-item">Calificacion: </li>-->
+                      
+            
        </div>
      </div>
   </div> 
+  <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Receta 1</h5>
+                <img src="../image/61205.png" alt="..." width="90" height="100">
+                <p class="card-text">Descripción de la receta 1.</p>
+               <!-- <a href="#" class="btn btn-primary">Visitar perfil</a>-->
+              <!-- <img src="../image/logoG.png" alt="...">-->
+                <a href="#" class="btn btn-primary">Visitar receta</a>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Receta 2</h5>
+                <img src="../image/61205.png" alt="..." width="90" height="100">
+                <p class="card-text">Descripción de la receta 2.</p>
+               <!-- <a href="#" class="btn btn-primary">Visitar perfil</a>-->
+                <a href="#" class="btn btn-primary">Visitar receta</a>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Receta 3</h5>
+                <img src="../image/61205.png" alt="..." width="90" height="100">
+                <p class="card-text">Descripción de la receta 3.</p>
+               <!-- <a href="#" class="btn btn-primary">Visitar perfil</a>-->
+                <a href="#" class="btn btn-primary">Visitar receta</a>
+            </div>
+        </div>
+    </div>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
 </script>
 </body>
