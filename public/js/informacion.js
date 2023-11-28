c4d97992f237fb41f044f1ca6399ef5fa956935f
Valentina Quiza <vquiza42@gmail.com>
2023-11-28 17:15:43 -0500

    act

--- public/js/informacion.js
+++ public/js/informacion.js
@@ -1,16 +1,28 @@
 function informacionReceta() {
     axios.get("receta")
-    .then(response => {
-        // Accede a la información de nombre y descripción desde la respuesta del servidor
-        const nombre = response.data.nombre;
-        const descripcion = response.data.descripcion;
-
-        // Haz lo que necesites con la información obtenida
-        console.log("Nombre de la receta:", nombre);
-        console.log("Descripción de la receta:", descripcion);
+    .then(res => {
+        console.log(res)
+        rec = "";
+        res.data.forEach((element, index) => {
+            rec += `
+             <div>
+            <div class="row d-flex justify-content-around col-12 bg-white " style="border-radius: 30px">
+                <div class="col-md-2 col-2  ">
+                    <img src="../image/R.jpeg" width="200" height="200" alt="...">
+                </div>
+                <div class="col-md-6 p-4 ">
+                    <h5 class="mt-0"></h5>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <a href="visrec" class="card-link">Visitar Receta</a>
+                </div>
+            </div>
+        </div>`
+        });
+        document.getElementById("tablaReceta").innerHTML = rec;
     })
     .catch(error => {
         console.error("Error al obtener la información de nombre y descripción:", error);
     });
+    document.getElementById("tablaReceta").innerHTML = rec;
 }
 informacionReceta();
