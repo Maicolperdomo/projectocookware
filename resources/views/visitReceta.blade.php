cb77673707837b5c7f66b210ecd8f22aae006c5a
Valentina Quiza <vquiza42@gmail.com>
2023-11-30 22:58:58 -0500

    a

--- resources/views/visitReceta.blade.php
+++ resources/views/visitReceta.blade.php
@@ -7,11 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" 
-    integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-  crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <title>detalle de Receta</title>
 </head>
 
@@ -30,22 +27,23 @@
                         <h5>Calificación</h5>
                     </div>
                     <div class="star-rating">
-  <input type="radio" id="star0"  class="fa far-star" data-index="0">
-  <label for="star0"></label>
-  <input type="radio" id="star1" class="fa far-star" data-index="1">
-  <label for="star1"></label>
-  <input type="radio" id="star2" class="fa far-star" data-index="2">
-  <label for="star2"></label>
-  <input type="radio" id="star3"  class="fa far-star" data-index="3">
-  <label for="star3"></label>
-  <input type="radio" id="star4"  class="fa far-star" data-index="4">
-  <label for="star4"></label>
-</div>
-                    </div>
+                        <input type="radio" id="star0"name="rating" class="fa far-star" data-index="0">
+                        <label for="star0"></label>
+                        <input type="radio" id="star1" name="rating" class="fa far-star" data-index="1">
+                        <label for="star1"></label>
+                        <input type="radio" id="star2"  name="rating"class="fa far-star" data-index="2">
+                        <label for="star2"></label>
+                        <input type="radio" id="star3" name="rating" class="fa far-star" data-index="3">
+                        <label for="star3"></label>
+                        <input type="radio" id="star4"  name="rating"class="fa far-star" data-index="4">
+                        <label for="star4"></label>
 
+                    </div>
                 </div>
+
             </div>
         </div>
+    </div>
     </div>
     </div>
     <div class="col-9 right scrollable-right content-s">
@@ -144,114 +142,121 @@
 
 </html>
 <script>
-    var ratedIndex = -1, uID =0;
+    var ratedIndex = -1,
+        uID = 0;
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         resetStarColors();
 
-        if(localStorage.getItem('ratedIndex') !=null){
-         setStars(parseInt(localStorage.getItem('ratedIndex')));
-         uID = localStorage.getItem('uID');
-    }
-$('.fa-star').on('click',function(){
-    ratedIndex = parseInt($(this).data('index'));
-    localStorage.setItem('ratedIndex', ratedIndex);
-    saveToTheDB();
-});
-$('.fa-star').mousemove(function(){
-    resetStarColors();
-    var currentIndex = parseInt($(this).data('index'));
-    setStars(currentIndex);
-});
-$('.fa-star').mouseleave(function(){
-    resetStarColors();
+        if (localStorage.getItem('ratedIndex') != null) {
+            setStars(parseInt(localStorage.getItem('ratedIndex')));
+            uID = localStorage.getItem('uID');
+        }
+        $('.fa-star').on('click', function() {
+            ratedIndex = parseInt($(this).data('index'));
+            localStorage.setItem('ratedIndex', ratedIndex);
+            saveToTheDB();
+        });
+        $('.fa-star').mousemove(function() {
+            resetStarColors();
+            var currentIndex = parseInt($(this).data('index'));
+            setStars(currentIndex);
+        });
+        $('.fa-star').mouseleave(function() {
+            resetStarColors();
 
-       if(ratedIndex !=-1)
-        setStars(ratedIndex)
-});
+            if (ratedIndex != -1)
+                setStars(ratedIndex)
+        });
 
     });
-    function saveToTheDB(){
+
+    function saveToTheDB() {
         $.ajax({
             url: "visitReceta.blade.php",
-            method : "POST",
+            method: "POST",
             dataType: 'json',
             data: {
                 save: 1,
                 uID: uID,
                 ratedIndex: ratedIndex
-            }, success: function(r){
-              uID: r.uid;
-              localStorage.setItem('uID', 'uID');
+            },
+            success: function(r) {
+                uID: r.uid;
+                localStorage.setItem('uID', 'uID');
             }
         })
     }
-    function setStars(max){
-        for (var i=0; i<= max; i++ )
-        $('.fa-star:eq('+i+')').css('color', 'green');
+
+    function setStars(max) {
+        for (var i = 0; i <= max; i++)
+            $('.fa-star:eq(' + i + ')').css('color', 'green');
     }
-    function resetStarColors(){
+
+    function resetStarColors() {
         $('.fa-star').css('color', 'black');
     }
 </script>
 <?php
 
 $conn = new mysqli('localhost', 'root', '', 'proyectococina');
-    if(isset($_POST['save'])){
+if (isset($_POST['save'])) {
     $uID = $conn->real_escape_string($_POST['uID']);
-    $ratedIndex =$conn-> real_escape_string($_POST['ratedIndex']);
+    $ratedIndex = $conn->real_escape_string($_POST['ratedIndex']);
     $ratedIndex++;
-   
-    if($uID){
+
+    if ($uID) {
         $conn->query(query: "INSERT TO stars (ratedIndex) VALUES('$ratedIndex')");
         $sql = $conn->query("SELECT id FROM stars ORDER BY id DESC LIMIT 1");
         $uData = $sql->fetch_assoc();
         $uID = $uData['id'];
-    }else
-    $conn->query(query:"UPDATE stars SET ratedIndex='$ratedIndex' WHERE id ='$uID'");
-    exit(json_encode(array('id'=>$uID)));
+    } else
+        $conn->query(query: "UPDATE stars SET ratedIndex='$ratedIndex' WHERE id ='$uID'");
+    exit(json_encode(array('id' => $uID)));
 }
-/**$sql=$conn->query(query:"SELECT id FROM stars");
+$sql = $conn->query(query: "SELECT id FROM stars");
 $numR = $sql->num_rows;
 
-$sql = $conn->query(query:"SELECT SUM(rateIndex) AS total FROM stars");
+$sql = $conn->query(query: "SELECT SUM(rateIndex) AS total FROM stars");
 $rData = $sql->fetch_array();
 $total = $rData['total'];
 
-$avg= $otal / $numR;/** */
+$avg = $total / $numR;
 
 ?>
 <style>
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  margin: 0;
-  background-color: #f4f4f4;
-}
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        margin: 0;
+        background-color: #f4f4f4;
+    }
 
-.star-rating {
-  font-size: 0;
-}
+    .star-rating {
+        font-size: 0;
+    }
 
-.star-rating input {
-  display: none;
-}
+    .star-rating input {
+        display: none;
+    }
 
-.star-rating label {
-  display: inline-block;
-  cursor: pointer;
-  font-size: 2rem;
-  color: #ccc;
-}
+    .star-rating label {
+        display: inline-block;
+        cursor: pointer;
+        font-size: 2rem;
+        color: #ccc;
+    }
 
-.star-rating label:before {
-  content: '\2605'; /* Código de estrella en unicode */
-  display: block;
-}
+    .star-rating label:before {
+        content: '\2605';
+        /* Código de estrella en unicode */
+        display: block;
+    }
 
-.star-rating input:checked ~ label {
-  color: #ffd700; /* Color de estrella seleccionada */
-}
+    .star-rating input:checked~label {
+        color: #ffd700;
+        /* Color de estrella seleccionada */
+    }
 </style>
